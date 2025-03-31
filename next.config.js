dc1de236f1aad3edc913e8ccf43d7bad0ca9b282
ativ2k <ativ.patel@gmail.com>
2025-03-30 19:36:38 -0700

    allow external image domains

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
     // Ignore ESLint errors during build (treat them as warnings instead)
     ignoreDuringBuilds: true
   },
+  images: {
+    domains: ['i.imgur.com', 'images.pexels.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com'
+      }
+    ]
+  },
   // Add other configuration options here as needed
 }
 
